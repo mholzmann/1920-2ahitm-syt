c129d2f6f7436f6783581e746a0d279d7d8e40b9
MichaelHolzmann <m.holzmann@htl-leonding.ac.at>
2020-02-25 09:38:59 +0100

    Simplfy encoding parameter

--- 09 file system/01 how to read a file/readFile.js
+++ 09 file system/01 how to read a file/readFile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const filename = "hello.txt";
 
-fs.readFile(filename, { encoding: 'utf8' }, (err, data) => {
+fs.readFile(filename, 'utf8', (err, data) => {
     if (err) {
         console.log(err.message);
     } else {
